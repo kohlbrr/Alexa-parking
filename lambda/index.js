1d2a73f5f6d96ac5c877ba0351cf8d103abcdd9d
kohlbrr <kohlbrr@gmail.com>
2017-06-19 01:52:28 -0400

    enhancement(index GET; speechAssets): created a helper function to allow better querying of ASP information by day from the 311 API; added intents and utterances to repo

--- lambda/index.js
+++ lambda/index.js
@@ -4,7 +4,7 @@ const Alexa = require('alexa-sdk')
 
 /*** HELPER FUNCTIONS ***/
 
-// Interpret GET requests w/o import
+// Alows GET requests
 const getContent = (url) => {
   return new Promise((resolve, reject) => {
     const lib = url.startsWith('https') ? require('https') : require('http')
@@ -20,27 +20,26 @@ const getContent = (url) => {
   })
 }
 
-const alternatePhoneme = `<phoneme alphabet="ipa" ph="ɔ: l t ɜ: n ə t  ">alternate</phoneme>`
-
-const addPhonemes = (res) => {
-  return res.replace(/[Aa]lternate/g, alternatePhoneme)
+// Gets a date string in consumable format for the 311 API
+const get311Date = (daysIntoFuture) => {
+  const date = new Date()
+  date.setDate(date.getDate() + daysIntoFuture)
+  const day = date.getDate()
+  const month = date.getMonth()+1
+  const year = date.getFullYear()
+  return (
+    ('0' + month).slice(-2) +
+    ('0' + day).slice(-2) +
+    year
+  )
 }
 
-/*** END HELPER FUNCTIONS ***/
-
+// URL target for ASP 311 API requests
 const getAspUrl = `https://api.cityofnewyork.us/311/v1/municipalservices?app_id=${process.env.ASP_311_APP_ID}&app_key=${process.env.ASP_311_APP_KEY}`
-const HELP_MESSAGE = `I will tell you if ${alternatePhoneme} Side Parking is in effect in New York City` // TODO
-const STOP_MESSAGE = 'Goodbye' // TODO
 
-const handlers = {
-  'LaunchRequest': function() {
-    this.emit('GetASPInfo')
-  },
-  'AlternateSideParking': function() {
-    this.emit('GetASPInfo')
-  },
-  'GetASPInfo': function() {
-    getContent(getAspUrl)
+// Gets ASP info for a specific date
+const getAspInfo = (date) => {
+    return getContent(getAspUrl + `&startDate=${date}`)
     .then(res => JSON.parse(res))
     .then(res => {
       return ({ // TODO: Clean this up
@@ -50,9 +49,35 @@ const handlers = {
       })
     })
     .then(res => {
-        console.log(res)
-      this.emit(':tell', addPhonemes(res.message))
+      return addPhonemes(res.message)
     })
+    .catch(console.error)
+}
+
+// Phoneme alphabet consts to replace in responses
+const alternatePhoneme = `<phoneme alphabet="ipa" ph="ɔ: l t ɜ: n ə t  ">alternate</phoneme>`
+
+// Replaces mispronounced words in responses with phoneme alphabet versions
+const addPhonemes = (res) => {
+  return res.replace(/[Aa]lternate/g, alternatePhoneme)
+}
+
+/*** END HELPER FUNCTIONS ***/
+
+const HELP_MESSAGE = `I will tell you if ${alternatePhoneme} Side Parking is in effect in New York City` // TODO
+const STOP_MESSAGE = 'Goodbye' // TODO
+
+const handlers = {
+  'LaunchRequest': function() {
+    this.emit('AlternateSideParking')
+  },
+  'AlternateSideParking': function() {
+    getAspInfo(get311Date(0))
+    .then(message => { this.emit(':tell', message) })
+  },
+  'AlternateSideTomorrow': function() {
+    getAspInfo(get311Date(1))
+    .then(message => { this.emit(':tell', ('For tomorrow, ' + message)) })
   },
   'AMAZON.HelpIntent': function() {
     const speechOutput = HELP_MESSAGE
